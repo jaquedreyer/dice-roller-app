a16bf5b0868f4621f0bd68190baae67ed67eae9a
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2023-05-04 17:39:45 -0300

    add unit test

--- app/src/test/java/com/dreyer/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/dreyer/diceroller/ExampleUnitTest.kt
@@ -9,9 +9,13 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+//As the rollResult are random numbers, the test only check if the rollResult is between the int range
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_numbers() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6 )
     }
 }
